79bc54cb5a0b0d88e420401ddfd214c46ea9ad6e
JEEWOOCHOI <jeewoochoi47@gmail.com>
2024-06-05 13:03:21 +0900

    가게에 리뷰 추가하기 API

--- NodeJS/src/controllers/rest.controller.js
+++ NodeJS/src/controllers/rest.controller.js
@@ -1,9 +1,16 @@
 import { status } from "../../config/resp.status.js";
 import { response } from "../../config/response.js";
 import { addRestaurant } from "../services/rest.service.js";
+import { addReview } from "../services/review.service.js";
 
 export const restAdd=async(req,res,next)=>{
     console.log("식당이 추가됐습니다!");
     console.log("body:",req.body);
     res.send(response(status.SUCCESS,await addRestaurant(req.body)));
+}
+
+export const reviewAdd=async(req,res,next)=>{
+    console.log("리뷰가 추가되었습니다!");
+    console.log("body:",req.body);
+    res.send(response(status.SUCCESS,await addReview(req.body)));
 }
--- NodeJS/src/routes/rest.route.js
+++ NodeJS/src/routes/rest.route.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { restAdd } from "../controllers/rest.controller.js";
+import { restAdd, reviewAdd } from "../controllers/rest.controller.js";
 
 export const restRouter=express.Router();
 restRouter.post('/add',restAdd);
+restRouter.post('/review',reviewAdd);
