a5c7300bc1d395ccc4faa5b28e9ddef527aeed75
JEEWOOCHOI <jeewoochoi47@gmail.com>
2024-06-22 00:06:25 +0900

    특정 가게의 미션 목록 조회하기 API

--- NodeJS/src/dtos/rest.dto.js
+++ NodeJS/src/dtos/rest.dto.js
@@ -5,3 +5,16 @@ export const addRestResponseDTO=(data)=>{
         'region_id':data[0].region_id
     };
 }
+
+export const restPreviewMissionResponseDTO=(data)=>{
+    const missions=[];
+    for(let i=0;i<data.length;i++){
+        missions.push({
+            "left_date":data[i].left_date,
+            "content": data[i].content,
+            "reward":data[i].reward,
+            "certification_num":data[i].certification_num
+        })
+    }
+    return {"missionData": missions,"cursorId":data[data.length-1].mission_id};
+}
--- NodeJS/src/models/user.dao.js
+++ NodeJS/src/models/user.dao.js
@@ -4,10 +4,6 @@ import { status } from "../../config/resp.status.js";
 import { getReviewByUserId, getReviewByUserIdAtFirst } from "./user.sql.js";
 
 export const getUserPreviewReview = async (cursorId,size,userId)=>{
-    console.log(cursorId);
-    console.log(size);
-    console.log(parseInt(userId));
-
     try {
         const conn = await pool.getConnection();
         if(cursorId == "undefined" || typeof cursorId == "undefined" || cursorId == null){
